0f99a0e7db2382afd36fd6b9efd2247a61fa7931
vladc <vlad.chrv>
2022-02-22 18:08:03 +0100

    export/import or how to share data between modules

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+
+import {q} from './learn'
+import {w} from './learn'
+import {i, a} from './learn'
+import Learn from './learn'
+
+console.log(`Parameter: q:${q} w:${w}`);
+console.log(`Parameter: (i)${i} + (a)${a} = ${i + a}`);
+
+console.log(`Parameter r: ${Learn.r}`)
 
 createApp(App).mount('#app')
