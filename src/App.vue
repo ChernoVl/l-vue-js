70d38a0965e9572a7ff83f11aae9c6e47c615bed
Vladyslav Chernov ext <Vladyslav.Chernov.ext@msg.group>
2022-03-04 19:28:15 +0100

    TODO List. events, emit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="App">
-
+    <TodoList :todos="todos" @delete-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import TodoList from "@/components/TodoList";
+
 export default {
   name: 'App',
+  data(){
+    return {
+      todos: ['Fixing', 'Breakfast']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods:{
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  }
 }
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="(todo, index) in todos" @click="deleteTodo(index)" :key="index"> o {{ todo }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['todos'],
+  methods: {
+    deleteTodo(index){
+      this.$emit('delete-todo', index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
